d02bf118a51f64541382d4b0d31f5d7d76835cb5
marianrabagel <rabagelmarian@gmail.com>
2015-06-17 12:43:44 +0300

    Update SumOfEvenNumbers.cs

--- 1/SumOfEvenNumbers.cs
+++ 1/SumOfEvenNumbers.cs
@@ -8,29 +8,17 @@ namespace AgileFreaks_test1
 {
     public class SumOfEvenNumbers
     {
-        private int s;
-
-        public SumOfEvenNumbers()
-        {
-            s = 0;
-        }
-
         public int computeEvenNumbersSum(int[] array)
         {
-            s = 0;
+            int s = 0;
             for (int i = 0; i < array.Length; i++)
                 if (isEven(array[i]))
-                    addToSum(array[i]);
+                    s += array[i];
             return s;
         }
         private bool isEven(int number)
         {
             return number % 2 == 0;
-                
-        }
-        private void addToSum(int number)
-        {
-            s += number;
         }
     }
 }
